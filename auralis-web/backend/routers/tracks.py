489d44f198c6460f814655c564ea0e71efa54e05
Skull0716 <diazever0716@gmail.com>
2026-07-29 15:32:37 -0600

    fix(security): add validate_file_path to lyrics endpoint (#4814)
    Signed-off-by: Skull0716 <diazever0716@gmail.com>

--- auralis-web/backend/routers/tracks.py
+++ auralis-web/backend/routers/tracks.py
@@ -148,6 +148,8 @@ def create_tracks_router(
 
             # Try to extract from file
             try:
+                from security.path_security import PathValidationError, validate_file_path
+                validate_file_path(str(track.filepath))
                 import mutagen
                 audio_file = await asyncio.to_thread(mutagen.File, track.filepath)  # type: ignore[attr-defined]
                 lyrics_text = None
